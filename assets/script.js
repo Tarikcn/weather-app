e89656498e8e0d2d7ef7eb788b626ddb0b3a6715
unknown <khamliche@gmail.com>
2022-12-31 12:27:24 +0000

    add 5dayforecasr code

--- assets/script.js
+++ assets/script.js
@@ -12,5 +12,72 @@ var cityHistory = [];
 function curentConditions(coord) {
   let lat = coord[0].lat.toString();
   let lon = coord[0].lon.toString();
-  let currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}${APIkey}&units=metric`;
+  let currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}${APIkey}`;
+  fetch(currentWeatherAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // city's name, and use moment to get the date
+      var city = data.name;
+      var date = moment().format(" DD/MM/YYYY");
+      // weather condition icon
+      var weatherIcon = data.weather[0].icon;
+      var urlIcon = "https://openweathermap.org/img/w/" + weatherIcon + ".png";
+      var cityHeader = $("<h2>").html(city + date);
+      todayContainer.addClass("card");
+      currentWeather.addClass("card-body");
+      currentWeather.prepend(cityHeader);
+      currentWeather.append($("<img>").attr("src", urlIcon));
+      // temp, feels like, humidity, windspeed
+      var temp = Math.ceil(data.main.temp);
+      currentWeather.append($("<p>").html("Temperature: " + temp + " &#8451"));
+      var feelsLike = Math.ceil(data.main.feels_like);
+      currentWeather.append(
+        $("<p>").html("Feels Like: " + feelsLike + " &#8451")
+      );
+      var humidity = data.main.humidity;
+      currentWeather.append($("<p>").html("Humidity: " + humidity + " %"));
+      var windSpeed = data.wind.speed;
+      currentWeather.append($("<p>").html("Wind Speed: " + windSpeed + " MPH"));
+    });
+}
+//fetch 5 days forecast api and display the next 5 days forecast onto the page
+function futureConditions(coord) {
+  let lat = coord[0].lat.toString();
+  let lon = coord[0].lon.toString();
+  let forecastAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}${APIkey}&units=metric`;
+
+  fetch(forecastAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 5; i < 38; i += 8) {
+        // Create a div for each forecast card
+        var weatherCard = $("<div>").attr(
+          "class",
+          "col five-days text-white rounded-lg p-2"
+        );
+        // variables
+        var list = data.list[i];
+        var main = data.list[i].main;
+        forecast5.append(weatherCard);
+        // Get the date and format using momentJs
+        cardDate = moment(list.dt_txt.split(" ")[0]).format("l");
+        // Display Date
+        weatherCard.append($("<h4>").html(cardDate));
+        // Display Icon
+        var iconCode = list.weather[0].icon;
+        var urlIcon = `https://openweathermap.org/img/w/${iconCode}.png`;
+        weatherCard.append($("<img>").attr("src", urlIcon));
+        // display temp, windspeed, and humidity
+        var temp = Math.ceil(main.temp);
+        weatherCard.append($("<p>").html("Temp: " + temp + " &#8451"));
+        var windSpeed = list.wind.speed;
+        weatherCard.append($("<p>").html("Wind Speed: " + windSpeed + " KPH"));
+        var humidity = main.humidity;
+        weatherCard.append($("<p>").html("Humidity: " + humidity + " %"));
+      }
+    });
 }
